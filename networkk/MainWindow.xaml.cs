e8d09905a5bc74eafd3fae70b93f12426768246f
RanyaAli20 <ranyaali.2000918@gmail.com>
2024-11-19 09:25:29 +0200

    runcode

--- networkk/MainWindow.xaml.cs
+++ networkk/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace networkk
+namespace GraphApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddNode_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق إضافة عقدة
+        }
+
+        private void AddEdge_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق إضافة حافة
+        }
+
+        private void DeleteNode_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق حذف عقدة
+        }
+
+        private void DeleteEdge_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق حذف حافة
+        }
+
+        private void ClearCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق مسح الشاشة
+        }
+
+        private void RunDijkstra_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق تنفيذ خوارزمية Dijkstra
+        }
+
+        private void RunBFS_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق تنفيذ خوارزمية BFS
+        }
+
+        private void RunDFS_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق تنفيذ خوارزمية DFS
+        }
+
+        private void RunPrim_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق تنفيذ خوارزمية Prim
+        }
+
+        private void RunMaxST_Click(object sender, RoutedEventArgs e)
+        {
+            // منطق تنفيذ الشجرة الممتدة الكبرى
         }
     }
 }
